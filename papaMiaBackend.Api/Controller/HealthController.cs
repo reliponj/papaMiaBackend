c481e518f5650f66e5aeee6fab89395ac8b3237c
reliponj <andrewwwsha@gmail.com>
2026-03-13 21:40:33 +0200

    Добавьте файлы проекта.

---
+++ papaMiaBackend.Api/Controller/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace papaMiaBackend.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
---
+++ papaMiaBackend.Api/Controller/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace papaMiaBackend.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
