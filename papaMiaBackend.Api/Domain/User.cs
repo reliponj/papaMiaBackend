2c749b7a2679b9cd2887dedf3c270a953e510a60
AlinaKernus <alinakernus2@gmail.com>
2026-03-15 17:08:51 +0200

    Added UserController and User

---
+++ papaMiaBackend.Api/Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace papaMiaBackend.Api.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt{ get; set; }
+    }
+}
