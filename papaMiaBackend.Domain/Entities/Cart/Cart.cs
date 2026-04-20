43c679b7570ef0f22e7987a2d5bd92a7e382bfda
reliponj <andrewwwsha@gmail.com>
2026-04-20 17:00:08 +0300

    Cart and Cart Item

---
+++ papaMiaBackend.Domain/Entities/Cart/Cart.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace papaMiaBackend.Domain.Entities.Cart;
+
+[Table("Carts")]
+public class Cart
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ papaMiaBackend.Domain/Entities/Cart/CartItem.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace papaMiaBackend.Domain.Entities.Cart;
+
+[Table("CartItems")]
+public class CartItem
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public int CartId { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; set; } = 1;
+}
