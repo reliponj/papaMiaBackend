2d7a853b813972ee942540a69ef69ba38dc985ec
AlinaKernus <alinakernus2@gmail.com>
2026-04-21 20:20:29 +0300

    fixed Banner mapping

--- papaMiaBackend.Api/Program.cs
+++ papaMiaBackend.Api/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<UserMappingProfile>());
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<ProductMappingProfile>());
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<CategoryMappingProfile>());
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<BannerMappingProfile>());
 builder.Services.AddScoped<BusinessLogicManager>();
 
 var app = builder.Build();
