5914223f5150dcab330e30b3ec9d5d279fad18f6
AlinaKernus <alinakernus2@gmail.com>
2026-05-05 11:25:31 +0300

    allergen entity and context

---
+++ papaMiaBackend.DataAccess/Migrations/Product/20260505082457_Allergen.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace papaMiaBackend.DataAccess.Migrations.Product
+{
+    /// <inheritdoc />
+    public partial class Allergen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Allergens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductAllergens",
+                columns: table => new
+                {
+                    AllergenLinksId = table.Column<int>(type: "integer", nullable: false),
+                    ProductsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductAllergens", x => new { x.AllergenLinksId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_ProductAllergens_Allergens_AllergenLinksId",
+                        column: x => x.AllergenLinksId,
+                        principalTable: "Allergens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductAllergens_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAllergens_ProductsId",
+                table: "ProductAllergens",
+                column: "ProductsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductAllergens");
+
+            migrationBuilder.DropTable(
+                name: "Allergens");
+        }
+    }
+}
