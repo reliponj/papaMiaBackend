cd1d1af16ef1a20cfc7ac000a4cb77c270432ffb
reliponj <andrewwwsha@gmail.com>
2026-05-05 12:42:39 +0300

    Promocode Usage and validation

--- papaMiaBackend.DataAccess/Context/PromocodeContext.cs
+++ papaMiaBackend.DataAccess/Context/PromocodeContext.cs
@@ -9,7 +9,24 @@ public class PromocodeContext : DbContext
         : base(options)
     {
     }
+
     public virtual DbSet<Promocode> Promocodes { get; set; }
+
+    public virtual DbSet<PromocodeUsage> PromocodeUsages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PromocodeUsage>()
+            .HasOne(u => u.Promocode)
+            .WithMany()
+            .HasForeignKey(u => u.PromocodeId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<PromocodeUsage>()
+            .HasIndex(u => new { u.UserId, u.PromocodeId })
+            .IsUnique();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
--- papaMiaBackend.DataAccess/Migrations/Promocode/20260505094144_Promocode Usage.Designer.cs
+++ papaMiaBackend.DataAccess/Migrations/Promocode/20260505094144_Promocode Usage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using papaMiaBackend.DataAccess.Context;
@@ -11,9 +12,11 @@ using papaMiaBackend.DataAccess.Context;
 namespace papaMiaBackend.DataAccess.Migrations.Promocode
 {
     [DbContext(typeof(PromocodeContext))]
-    partial class PromocodeContextModelSnapshot : ModelSnapshot
+    [Migration("20260505094144_Promocode Usage")]
+    partial class PromocodeUsage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
