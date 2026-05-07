b95a8ac394a6392e032987779393fc66a6229261
AlinaKernus <alinakernus2@gmail.com>
2026-05-07 12:40:16 +0300

    order dtos

---
+++ papaMiaBackend.Domain/Models/Order/OrderCreateDto.cs
@@ -0,0 +1,17 @@
+using papaMiaBackend.Domain.Entities.Order;
+
+namespace papaMiaBackend.Domain.Models.Order;
+
+public class OrderCreateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string District { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string? Note { get; set; }
+    public OrderPaymentKind PaymentKind { get; set; }
+    public OrderCardProvider? CardProvider { get; set; }
+    public List<OrderCreateItemDto> Items { get; set; } = [];
+}
---
+++ papaMiaBackend.Domain/Models/Order/OrderDto.cs
@@ -0,0 +1,21 @@
+using papaMiaBackend.Domain.Entities.Order;
+
+namespace papaMiaBackend.Domain.Models.Order;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string District { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string? Note { get; set; }
+    public OrderPaymentKind PaymentKind { get; set; }
+    public OrderCardProvider? CardProvider { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public OrderStatus Status { get; set; }
+    public List<OrderItemDto> Items { get; set; } = [];
+}
