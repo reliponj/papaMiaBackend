dd2770d9a5657a15d4781ba6ecf5b50045a2f0ea
reliponj <andrewwwsha@gmail.com>
2026-05-21 18:39:42 +0300

    article comments bl

--- papaMiaBackend.Api/Controller/ArticleController.cs
+++ papaMiaBackend.Api/Controller/ArticleController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using papaMiaBackend.Api.Auth;
+using papaMiaBackend.Api.Swagger;
 using papaMiaBackend.BusinessLogic;
 using papaMiaBackend.BusinessLogic.Interfaces;
+using papaMiaBackend.Domain.Models.Article;
 
 namespace papaMiaBackend.Api.Controller;
 
@@ -9,10 +12,12 @@ namespace papaMiaBackend.Api.Controller;
 public class ArticleController : ControllerBase
 {
     private readonly IArticleAction _article;
+    private readonly ICurrentUser _currentUser;
 
-    public ArticleController(BusinessLogicManager bl)
+    public ArticleController(BusinessLogicManager bl, ICurrentUser currentUser)
     {
         _article = bl.ArticleAction();
+        _currentUser = currentUser;
     }
 
     [HttpGet]
@@ -30,5 +35,22 @@ public class ArticleController : ControllerBase
             return NotFound(new { message = "article_not_found" });
 
         return Ok(item);
+    }
+
+    [SwaggerBearer]
+    [HttpPost("{id:int}/comments")]
+    public IActionResult AddComment(int id, [FromBody] ArticleCommentCreateDto dto)
+    {
+        if (!_currentUser.TryGetUserId(out var userId))
+            return Unauthorized();
+
+        if (string.IsNullOrWhiteSpace(dto?.Text))
+            return BadRequest(new { message = "comment_text_required" });
+
+        var comment = _article.AddArticleCommentAction(id, userId, dto!);
+        if (comment is null)
+            return NotFound(new { message = "article_not_found" });
+
+        return Ok(comment);
     }
 }
